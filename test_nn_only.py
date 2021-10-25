299011d8983af7cb13242feef5227b47d1109008
tersekmatija <mt2421@student.uni-lj.si>
2021-10-25 21:27:19 +0200

    Remove USB transfer lag

--- test_nn_only.py
+++ test_nn_only.py
@@ -17,6 +17,8 @@ parser.add_argument('-nn', '--model_name', type=str, default=None, help="Name of
 parser.add_argument('-zoo', '--zoo_type', type=str, default=None, help="Zoo type")
 parser.add_argument('-shape', '--input_shape', type=int, nargs='+', help="List of ints", required=True)
 parser.add_argument('-fp16', '--fp16', action="store_true", help="Input must be FP16")
+parser.add_argument('-c', '--cache', action="store_true", help="Use cache with blobconverter.")
+
 args = parser.parse_args()
 
 print("Parsed arguments")
@@ -29,12 +31,16 @@ pipeline = dai.Pipeline()
 # NeuralNetwork
 print("Creating Neural Network...")
 detection_nn = pipeline.createNeuralNetwork()
-detection_nn.setBlobPath(str(blobconverter.from_zoo(name=args.model_name, zoo_type=args.zoo_type, shaves=args.shaves, use_cache = False)))
+detection_nn.setBlobPath(str(blobconverter.from_zoo(name=args.model_name, zoo_type=args.zoo_type,
+                                                    shaves=args.shaves, use_cache = args.cache)))
 detection_nn.setNumInferenceThreads(1)
+detection_nn.input.setBlocking(True)
+#detection_nn.out.setBlocking(True)
 
 nn_in = pipeline.createXLinkIn()
 nn_in.setStreamName("in_nn")
 nn_in.out.link(detection_nn.input)
+#nn_in.out.setBlocking(True)
 
 # Create outputs
 xout_nn = pipeline.createXLinkOut()
@@ -44,11 +50,40 @@ detection_nn.out.link(xout_nn.input)
 # Pipeline defined, now the device is assigned and pipeline is started
 with dai.Device(pipeline) as device:
 
-    detection_in = device.getInputQueue("in_nn")
-    q_nn = device.getOutputQueue(name="nn", maxSize=1, blocking=True)
+    detection_in = device.getInputQueue("in_nn", maxSize=25, blocking=True)
+    q_nn = device.getOutputQueue(name="nn", maxSize=5, blocking=True)
 
     fps_storage = []
 
+    for repetition in range(5):
+
+        print(f"Repetition: {repetition}")
+
+        # feed 55 messages
+        for i in range(25):
+            if not args.fp16:
+                frame = np.random.randint(256, size=args.input_shape, dtype=int)
+                nn_data = dai.NNData()
+                nn_data.setLayer("input", frame)
+            else:
+                frame = np.random.rand(*args.input_shape)
+                frame = frame.astype(np.float16).flatten().tolist()
+                nn_data = dai.Buffer()
+                nn_data.setData(frame)
+
+            detection_in.send(nn_data)
+
+        for i in range(5):
+            q_nn.get().getFirstLayerFp16()
+
+        start = time.time()
+        for i in range(20):
+            q_nn.get().getFirstLayerFp16()
+        diff = time.time() - start
+
+        fps_storage.append(20/diff)
+
+    """
     while len(fps_storage) <= 30:
 
         if not args.fp16:
@@ -69,5 +104,6 @@ with dai.Device(pipeline) as device:
 
         fps_storage.append(1 / diff)
         print(1 / diff)
+    """
 
-    print(np.mean(fps_storage[5:]), np.std(fps_storage[5:]))
+    print(np.mean(fps_storage), np.std(fps_storage), fps_storage)
